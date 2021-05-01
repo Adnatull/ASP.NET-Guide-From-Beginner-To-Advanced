885c9fc4383f20ba6521904730bf5f2cfc794bac
Adnatull Al Masum <adnaan.masum@yahoo.com>
2021-05-02 02:47:28 +0600

    AccessModifiers

---
+++ 01OOP/03AccessModifiers/03AccessModifiers/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03AccessModifiers
+{
+    public class Car
+    {
+        private string model1 = "Mustang";
+        protected string model3 = "Toyota";
+
+        public string model2 = "Ford";
+        
+        internal string model4 = "Tata";
+    }
+
+    public class DerivedCar : Car
+    {
+        public DerivedCar()
+        {
+            base.model3 = "Corola";
+        }
+        public string Type { get; set; }
+        
+    }
+}
---
+++ 01OOP/03AccessModifiers/03AccessModifiers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace _03AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var car = new Car();
+            Console.WriteLine(car);
+            var dcar = new DerivedCar();
+
+        }
+    }
+}
